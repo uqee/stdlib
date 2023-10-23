86c98ee18d0aacb34887ef760fa51d63be0e2474
Denis Zhbankov <denis.zhbankov@gmail.com>
2023-10-23 19:11:31 -0300

    master: optional log level

--- src/toLogLevel.ts
+++ src/toLogLevel.ts
@@ -1,5 +1,18 @@
+/* eslint-disable no-redeclare */
+
+import { Integer } from './Integer'
 import { LogLevel } from './LogLevel'
 
-export const toLogLevel = (logLevel: string): LogLevel => {
-  return parseInt(logLevel, 10) as LogLevel
+export function toLogLevel(logLevel: undefined): undefined
+export function toLogLevel(logLevel: string): LogLevel
+export function toLogLevel(logLevel: string | undefined): LogLevel | undefined {
+  if (logLevel === undefined) return undefined
+
+  const i: Integer = parseInt(logLevel, 10)
+  if (Number.isNaN(i)) return undefined
+
+  return Math.max(
+    Math.min(i, LogLevel._6_SPAM), //
+    LogLevel._0_SILENT,
+  ) as LogLevel
 }
